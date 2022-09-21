0dba64bcaade37bc3fe9e70f2ceda170a91bd05e
Leos <10955439@uvu.edu>
2022-09-20 20:01:16 -0600

    yums

--- Unit_01c/Loopies & Arrests.cs
+++ Unit_01c/Loopies & Arrests.cs
@@ -7,5 +7,9 @@ public class Program
 		string[] breads = { "White", "Whole Wheat", "Muffins", "Baguette", "Toast", "Cake", "Pan Dulce", "Cornbread", "Bagels", "Donuts", "Banana Bread", "Waffle", "Biscotti", "Biscuit", "Rolls", "Crepe", "Pancake", "Foccacia", "Tortilla" };
 		Console.WriteLine(breads.Length);
 		Console.WriteLine(breads[5] + " " + breads[15]);
+		foreach (var bread in breads)
+		{
+			Console.WriteLine("All hail the almighty " + bread);
+		}
 	}
 }
