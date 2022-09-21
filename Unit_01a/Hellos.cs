a38fc8a82f5bc8a14f5e4d7aab3393ea2cb9c3e6
Leos <10955439@uvu.edu>
2022-09-20 18:47:18 -0600

    Meep

---
+++ Unit_01a/Hellos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mom_Come_Pick_Me_Up_I_m_Scared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
