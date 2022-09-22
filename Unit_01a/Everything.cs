3cee0748938596a76e7afa16d33e3df7df0c6b21
Leos <10955439@uvu.edu>
2022-09-21 20:50:36 -0600

    Files

---
+++ Unit_01a/Everything.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace I_require_assistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Let's try the comment thingy 
+            // Success!!
+            // Reminder to commit all and push 
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Kethup and Mustard");
+            Console.WriteLine("Potatoes");
+            const int number = 15;
+            Console.WriteLine(number);
+
+            string name = "Toaster";
+            Console.WriteLine(name);
+            Console.WriteLine(number + " " + name);
+
+            string kitchens = "Oven";
+            string bestAppliance = (name + " " + kitchens);
+            Console.WriteLine(bestAppliance + "   " + bestAppliance);
+
+            long bigNum = 36543535873735L;
+            Console.WriteLine(bigNum);
+            // long for big numbers, int for small numbers
+            Console.WriteLine("Air");
+
+            bool isCSharpFun = true;
+            bool isFishTasty = true;
+            Console.WriteLine(isCSharpFun);
+            Console.WriteLine(isFishTasty);
+
+        }
+    }
+}
