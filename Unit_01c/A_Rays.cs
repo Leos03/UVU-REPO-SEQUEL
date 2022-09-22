b01f57aa60f01216555a7b029fe1c8ba59bb7fde
Leos <10955439@uvu.edu>
2022-09-21 21:47:57 -0600

    Added the array of carbs

--- Unit_01c/A_Rays.cs
+++ Unit_01c/A_Rays.cs
@@ -5,3 +5,4 @@ public class Program
 	public void Main()
 	{
 		// Getting lots of deja vu from this
+		string[] breads = { "White", "Whole Wheat", "Muffins", "Baguette", "Toast", "Cake", "Pan Dulce", "Cornbread", "Bagels", "Donuts", "Banana Bread", "Waffle", "Biscotti", "Biscuit", "Rolls", "Crepe", "Pancake", "Foccacia", "Tortilla" };
