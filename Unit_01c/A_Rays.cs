60de8cce4d1a295dd7e1cd8892ccefaffe9741c8
Leos <10955439@uvu.edu>
2022-09-21 21:51:03 -0600

    How many breads do I have

--- Unit_01c/A_Rays.cs
+++ Unit_01c/A_Rays.cs
@@ -6,3 +6,8 @@ public class Program
 	{
 		// Getting lots of deja vu from this
 		string[] breads = { "White", "Whole Wheat", "Muffins", "Baguette", "Toast", "Cake", "Pan Dulce", "Cornbread", "Bagels", "Donuts", "Banana Bread", "Waffle", "Biscotti", "Biscuit", "Rolls", "Crepe", "Pancake", "Foccacia", "Tortilla" };
+		breads[4] = "HOLY TOAST";
+		Console.WriteLine(breads[4]);
+		Console.WriteLine(breads.Length);
+	}
+}
