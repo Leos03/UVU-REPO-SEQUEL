c7d7bbff795dcd5acca387a9bc8839357c89a971
Leos <10955439@uvu.edu>
2022-09-23 23:00:25 -0600

    I'm going to have to trust dotnetfiddle

--- Unit_01c/Mini_Game_Slash_Challenge.cs
+++ Unit_01c/Mini_Game_Slash_Challenge.cs
@@ -3,3 +3,37 @@
 public class Program
 {
 	public void Main()
+	{
+		static void Main(string[] args)
+		{
+			string secretWord = "Toaster";
+			string guess = "";
+			int guessCount = 0;
+			int guessLimit = 5;
+			bool noGueses = false;
+			
+			while(guess ! = secretWord && noGueses)
+			{
+				if guessCount < guessLimit
+				{
+					Console.WriteLine("Insert Guess >:) : ");
+					guess = Console.ReadLine();
+					guessCount++;
+				}
+				else
+				{
+					noGueses = true;
+				}
+				
+			}
+			if (noGueses)
+			{
+				Console.WriteLine("oof...");
+			}
+			else
+			{
+				Console.WriteLine("Yays");
+			}
+		}
+	}
+}
