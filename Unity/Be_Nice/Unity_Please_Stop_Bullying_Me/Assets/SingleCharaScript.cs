eb218119423aa9f601adf3bd3ccb2e79a340e7fc
Leos <10955439@uvu.edu>
2022-10-04 21:42:22 -0600

    SWORDS

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/SingleCharaScript.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/SingleCharaScript.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class SingleCharaScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Stuff
     {
-        
+        public int arrow
+        public int staff;
+        public int sword;
+
+        public Stuff(int arr, int sta, int swo)
+        {
+            arrow = arr;
+            staff = sta;
+            sword = swo;
+        }
     }
 
-    // Update is called once per frame
+    public Stuff mystuff = new Stuff(10, 7, 25);
+    public float speed;
+    public float turnSpeed;
+    public Rigidbody arrowPrefab;
+    public Transform firePosition;
+    public float arrowSpeed;
+
     void Update()
     {
-        
+        Movement();
+        Shoot();
+    }
+
+
+    void Movement ()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+
+
+    void shoot
+    {
+        if(Input.GetButtonDown("Fire1") && myStuff.arrows > 0)
+        {
+            Rigidbody arrowInstance = Instantiate(arrowPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+    arrowInstance.AddForce(firePosition.forward* arrowSpeed);
+            myStuff.arrows--;
+        }
     }
 }
