f7ec54290422728da849fb82effc67342907059c
Leos <10955439@uvu.edu>
2022-10-04 23:54:17 -0600

    DONE TIME FOR SLEEP

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/ConvoScript.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/ConvoScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ConvoScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int intelligence = 5;
 
-    // Update is called once per frame
-    void Update()
+
+    void Greet()
     {
-        
+        switch (intelligence)
+        {
+            case 5:
+                print("Why hello there good sir! Let me teach you about Trigonometry!");
+                break;
+            case 4:
+                print("Hello and good day!");
+                break;
+            case 3:
+                print("Whadya want?");
+                break;
+            case 2:
+                print("Grog SMASH!");
+                break;
+            case 1:
+                print("Ulg, glib, Pblblblblb");
+                break;
+            default:
+                print("Incorrect intelligence level.");
+                break;
+        }
     }
 }
