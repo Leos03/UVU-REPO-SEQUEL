2aee2b2ccf9301b61996f70990a8ea877d3c0b75
Leos <10955439@uvu.edu>
2022-10-04 19:31:28 -0600

    Rigged

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/MouseClick.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/MouseClick.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MouseClick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        rb.AddForce(-transform.forward * 500f);
+        rb.useGravity = true;
     }
 }
