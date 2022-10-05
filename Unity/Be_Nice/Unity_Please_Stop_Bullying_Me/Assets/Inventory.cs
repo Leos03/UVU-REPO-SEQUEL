fae3758b9e2bc6680d4f66f699e6dfe3955b6a02
Leos <10955439@uvu.edu>
2022-10-04 22:06:26 -0600

    Staffs

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/Inventory.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/Inventory.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Stuff
     {
-        
+        public int arrows;
+        public int staff;
+        public int sword;
+        public float energy;
+
+        public Stuff(int arr, int sta, int swo)
+        {
+            arrow = arr;
+            staff = sta;
+            sword = swo;
+        }
+
+        public Stuff(int arr, float en)
+        {
+            arrow = arr;
+            energy = en;
+        }
+
+        // Constructor
+        public Stuff()
+        {
+            arrow = 1;
+            staff = 1;
+            sword = 1;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    // Creating an Instance (an Object) of the Stuff class
+    public Stuff myStuff = new Stuff(50, 5, 5);
+
+    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+
+    void Start()
     {
-        
+        Debug.Log(myStuff.bullets);
     }
 }
