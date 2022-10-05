a1ba3eb2d5436f5b46384ebf559b741eed285e75
Leos <10955439@uvu.edu>
2022-10-04 18:53:11 -0600

    AAAAAAAAAAAAAAQA

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/AnotherClass.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/AnotherClass.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class AnotherClass : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int apples;
+    public int bananas;
+
+
+    private int stapler;
+    private int sellotape;
+
+
+    public void FruitMachine(int a, int b)
     {
-        
+        int answer;
+        answer = a + b;
+        Debug.Log("Fruit total: " + answer);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OfficeSort(int a, int b)
     {
-        
+        int answer;
+        answer = a + b;
+        Debug.Log("Office Supplies total: " + answer);
     }
 }
