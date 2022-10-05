0f60bba96bbcb35b1136d198232a528ebc11921a
Leos <10955439@uvu.edu>
2022-10-04 19:42:21 -0600

    Yay countdowns

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/DeltaTime.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/DeltaTime.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DeltaTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 8f;
+    public float countdown = 3.0f;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)
+            light.enabled = true;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
     }
 }
