95bce75e38b82baaae05574f6f35af7aa3534a1b
Leos <10955439@uvu.edu>
2022-10-04 19:02:35 -0600

    The Monarchy
    is no more

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/CheckState.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/CheckState.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CheckState : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject myObject;
+
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Active Self: " + myObject.activeSelf);
+        Debug.Log("Active in Hierarchy" + myObject.activeInHierarchy);
     }
 }
