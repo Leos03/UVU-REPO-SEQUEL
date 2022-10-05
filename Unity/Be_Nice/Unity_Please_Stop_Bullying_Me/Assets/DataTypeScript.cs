1095d570791c24f4239d05ca925cb29e731527ee
Leos <10955439@uvu.edu>
2022-10-04 19:44:58 -0600

    hhhhhhhhhhh

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/DataTypeScript.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/DataTypeScript.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class DataTypeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        //Value type var
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Ref type var
+        Transform tran = transform;
+        tran.position = new Vector3(0, 2, 0);
     }
 }
