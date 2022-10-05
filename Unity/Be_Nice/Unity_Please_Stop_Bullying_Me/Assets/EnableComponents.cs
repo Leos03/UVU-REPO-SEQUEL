744be462e746902337d07d591bb724d7fb1daaad
Leos <10955439@uvu.edu>
2022-10-04 18:59:51 -0600

    Lights...

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/EnableComponents.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/EnableComponents.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class EnableComponents : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Light myLight;
+
+
     void Start()
     {
-        
+        myLight = GetComponent<Light>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
     }
 }
