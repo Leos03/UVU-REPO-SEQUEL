db6a6cafa1a03a2a8672297f99d08c51b9047bda
Leos <10955439@uvu.edu>
2022-10-04 19:27:58 -0600

    aklsdjbfglajksdfhlkasj

--- Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/AxisExample.cs
+++ Unity/Be_Nice/Unity_Please_Stop_Bullying_Me/Assets/AxisExample.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class AxisExample : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float range;
+    public Text textOutput;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: " + h.ToString("F2");
     }
 }
