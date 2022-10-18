c14a3374e52253286eb8e2fd9ddce26d8039fe0c
Leos <10955439@uvu.edu>
2022-10-18 16:34:58 -0600

    I oofed for a sec

--- Unity/FloatData_ScriptableObjects/Floats/Assets/Scenes/MySO.cs
+++ Unity/FloatData_ScriptableObjects/Floats/Assets/Scenes/MySO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scriptable_Ob : MonoBehaviour
+public class MySO : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
