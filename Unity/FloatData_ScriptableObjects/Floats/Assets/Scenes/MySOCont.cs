bf9f2ec03561c3ef5c54e0983dc9ff490d68958c
Leos <10955439@uvu.edu>
2022-10-18 18:31:46 -0600

    Spelling hard

--- Unity/FloatData_ScriptableObjects/Floats/Assets/Scenes/MySOCont.cs
+++ Unity/FloatData_ScriptableObjects/Floats/Assets/Scenes/MySOCont.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MySOCont : MonoBehaviour
+public class MySoCont : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Card card;
+
+	public Text nameText;
+	public Text descriptionText;
+
+	public Image artworkImage;
+
+	public Text manaText;
+	public Text attackText;
+	public Text healthText;
+
+	// Use this for initialization
+	void Start()
+	{
+		nameText.text = card.name;
+		descriptionText.text = card.description;
+
+		artworkImage.sprite = card.artwork;
+
+		manaText.text = card.manaCost.ToString();
+		attackText.text = card.attack.ToString();
+		healthText.text = card.health.ToString();
+	}
+
 }
