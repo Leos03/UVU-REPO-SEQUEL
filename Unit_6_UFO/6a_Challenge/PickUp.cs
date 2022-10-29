2c3af6f2c854eab2bbb12b9f2a2288b4d434f04b
Leos <10955439@uvu.edu>
2022-10-29 00:35:21 -0600

    Making a new folder just for the pickup because I will break Unity

---
+++ Unit_6_UFO/6a_Challenge/PickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public float speed = 50.0f;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
