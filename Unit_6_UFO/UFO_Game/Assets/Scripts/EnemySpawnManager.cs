a9f36b266d2e488e601aa9b988a487247e9a72c5
Leos <10955439@uvu.edu>
2022-11-01 18:43:33 -0600

    qaserhkhkjhsadfjhsdfjhsadfhsdafjhsdfahsdfajh

--- Unit_6_UFO/UFO_Game/Assets/Scripts/EnemySpawnManager.cs
+++ Unit_6_UFO/UFO_Game/Assets/Scripts/EnemySpawnManager.cs
@@ -9,14 +9,24 @@ public class EnemySpawnManager : MonoBehaviour
 
     private float spawnPosZ = 20f;
 
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX), 0, spawnPosZ);
-            int ufoIndex = Random.Range(0, ufoPrefabs.Length);
-            Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
-        }
+        
+    }
+
+    void SpawnRandomUFO()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int ufoIndex = Random.Range(0, ufoPrefabs.Length);
+        Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
     }
 }
