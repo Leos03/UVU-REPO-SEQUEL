ed3ab6551a7740bff3edeabbda444e19bd23cde4
Leos <10955439@uvu.edu>
2022-11-06 20:47:10 -0700

    nom

--- Unit_6_UFO/UFO_Game/Assets/Scripts/CollisionDetect.cs
+++ Unit_6_UFO/UFO_Game/Assets/Scripts/CollisionDetect.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class CollisionDetect : MonoBehaviour
 {
+    public ScoreManager scoreManager;
+    public int scoreToGive;
+
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        scoreManager.IncreaseScore(scoreToGive);
         Destroy(gameObject);
         Destroy(other.gameObject);
     }
